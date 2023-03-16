f538cf21390c85cea9df8760ad44fbf9fdd3cd45
André <andre.kreutzer@outlook.com>
2023-03-15 22:40:02 -0300

    Validating web page title

--- cypress/e2e/home.cy.js
+++ cypress/e2e/home.cy.js
@@ -1,5 +1,7 @@
 describe('empty spec', () => {
-  it('passes', () => {
+  it('Webapp should be online', () => {
     cy.visit('http://localhost:8080')
+
+    cy.title().should('eq', 'Gerencie suas tarefas com Mark L')
   })
 })
